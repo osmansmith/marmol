a0b4f7fe4aabf0798bdfd810000fdb0c9065492f
osmansmith <osman_ahumada@hotmail.com>
2016-04-05 03:47:27 -0300

    CATALOGO
    que tal?

--- granito.php
+++ granito.php
@@ -1,5 +1,34 @@
 <?php include_once "head.php";?>
 
 <!-- contenido de granito-->
+<div class="ui two column grid">
+<div class="column">
+    <div class="ui raised segment">
+      <a class="ui green ribbon label">GRANITO</a>
+      
+      <div style="margin-top:3%; "class="ui tiny images">
+  <img class="ui image" src="img/image.png">
+  <img class="ui image" src="img/image.png">
+  <img class="ui image" src="img/image.png">
+  <img class="ui image" src="img/image.png">
+   <img class="ui image" src="img/image.png">
+  <img class="ui image" src="img/image.png">
+</div>
+      
+      <p><br>
+      Marmolería SERENA MARMOL es una empresa que nace el año 2012, bajo la necesidad del mercado, fabricando piezas elaboradas en mármol, granito y cuarzo, dimensionados para el hogar en muebles, cocina y baño, ademas , para locales comerciales, inmobiliaria y otros. <br><br> Se elaboran cubiertas de mesas, revestimientos de cocinas, cubiertas para vanitorios, piso y revestimientos, entre otros. Administrada por su dueño, Segundo Hernández y Compa. Nuestros productos consisten en Mármol, Granito y Cuarzo, muebles a medidas. </p>
+      
+      
+      
+                                
+    </div>
+  </div>
+  <div class="column">
+     <div style="padding:5% 0 5% 0;" class="ui raised segment">
+   <center><img class="ui large image" src="img/image.png">
+ </center>
+      </div>
+  </div>
+</div>
 
 <?php include_once "footer.php";?>
