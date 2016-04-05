83757f9c662c55d97d30589f8d4784d929c6bfc2
osmansmith <osman_ahumada@hotmail.com>
2016-04-05 07:02:15 -0300

    marmo
    l :+1:

--- marmol.php
+++ marmol.php
@@ -1,133 +1,4 @@
 <?php include_once "head.php";?>
-
-<<<<<<< HEAD
-<div class="ui raised segment">
-      <a class="ui green ribbon label">Contactanos</a>
-      <span>Titulo</span>
-      
-      <p><br>
-      Estamos ubicados en <br>
-      Ruta D-43, Kilometro 12, Numero 5000 <br>
-      Sector Pan de Azúcar, <br>
-      Coquimbo, IV Regió.
-      </p>
-      
-      
-      
-      <div class="ui two column middle aligned very relaxed stackable grid">
-  <div class="column">
-  <p>Administración y Ventas <br>
-         Fono : +569 50047894 <br>
-         contacto@serenamarmol.cl
-      </p>
-  </div>
-  <div class="ui vertical divider"></div>
-  <div class="center aligned column">
-   <p> Gerencia de operaciones <br>
-      Fono : +569 50047696 <br>
-      shernandez@serenamarmol.cl
-   </p>
-  </div>
-</div>                              
-    </div>
-
-    cuarzoo
-
-    <div class="ui raised segment">
-      <a class="ui green ribbon label">Contactanos</a>
-      <span>Titulo</span>
-      
-      <p><br>
-      Estamos ubicados en <br>
-      Ruta D-43, Kilometro 12, Numero 5000 <br>
-      Sector Pan de Azúcar, <br>
-      Coquimbo, IV Regió.
-      </p>
-      
-      
-      
-      <div class="ui two column middle aligned very relaxed stackable grid">
-  <div class="column">
-  <p>Administración y Ventas <br>
-         Fono : +569 50047894 <br>
-         contacto@serenamarmol.cl
-      </p>
-  </div>
-  <div class="ui vertical divider"></div>
-  <div class="center aligned column">
-   <p> Gerencia de operaciones <br>
-      Fono : +569 50047696 <br>
-      shernandez@serenamarmol.cl
-   </p>
-  </div>
-</div>                              
-    </div>
-
-    granitooo
-
-    <div class="ui raised segment">
-      <a class="ui green ribbon label">Contactanos</a>
-      <span>Titulo</span>
-      
-      <p><br>
-      Estamos ubicados en <br>
-      Ruta D-43, Kilometro 12, Numero 5000 <br>
-      Sector Pan de Azúcar, <br>
-      Coquimbo, IV Regió.
-      </p>
-      
-      
-      
-      <div class="ui two column middle aligned very relaxed stackable grid">
-  <div class="column">
-  <p>Administración y Ventas <br>
-         Fono : +569 50047894 <br>
-         contacto@serenamarmol.cl
-      </p>
-  </div>
-  <div class="ui vertical divider"></div>
-  <div class="center aligned column">
-   <p> Gerencia de operaciones <br>
-      Fono : +569 50047696 <br>
-      shernandez@serenamarmol.cl
-   </p>
-  </div>
-</div>                              
-    </div>
-
-
-    Gold!!
-
-    <div class="ui raised segment">
-      <a class="ui green ribbon label">Contactanos</a>
-      <span>Titulo</span>
-      
-      <p><br>
-      Estamos ubicados en <br>
-      Ruta D-43, Kilometro 12, Numero 5000 <br>
-      Sector Pan de Azúcar, <br>
-      Coquimbo, IV Regió.
-      </p>
-      
-      
-      
-      <div class="ui two column middle aligned very relaxed stackable grid">
-  <div class="column">
-  <p>Administración y Ventas <br>
-         Fono : +569 50047894 <br>
-         contacto@serenamarmol.cl
-      </p>
-  </div>
-  <div class="ui vertical divider"></div>
-  <div class="center aligned column">
-   <p> Gerencia de operaciones <br>
-      Fono : +569 50047696 <br>
-      shernandez@serenamarmol.cl
-   </p>
-  </div>
-</div>                              
-    </div>
-=======
 <!-- contenido de mármol-->
 <div class="ui two column grid">
 <div class="column">
@@ -149,12 +20,12 @@
       </div>
   </div>
   <div class="column">
-     <div style="padding:5% 0 5% 0;" class="ui raised segment">
-   <center><img id="mostrar" class="ui huge image autumn leaf" src="img/image.png">
+     <div  class="ui raised segment">
+   <center><img id="mostrar" class="ui image autumn leaf" src="img/image.png">
  </center>
       </div>
   </div>
 </div>
->>>>>>> origin/master
+
 
 <?php include_once "footer.php";?>
