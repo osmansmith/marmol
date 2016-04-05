f4e860cf867b7c87e08942615e0c5aa0345e56a8
CuscarisunWeb <jean.veliz.cortes@gmail.com>
2016-04-05 04:00:43 -0300

    Template_050416
    Servicios

--- cuarzo.php
+++ cuarzo.php
@@ -1,5 +1,130 @@
 <?php include_once "head.php";?>
 
-<!-- contenido de cuarzo-->
+<div class="ui raised segment">
+      <a class="ui green ribbon label">Contactanos</a>
+      <span>Titulo</span>
+      
+      <p><br>
+      Estamos ubicados en <br>
+      Ruta D-43, Kilometro 12, Numero 5000 <br>
+      Sector Pan de Azúcar, <br>
+      Coquimbo, IV Regió.
+      </p>
+      
+      
+      
+      <div class="ui two column middle aligned very relaxed stackable grid">
+  <div class="column">
+  <p>Administración y Ventas <br>
+         Fono : +569 50047894 <br>
+         contacto@serenamarmol.cl
+      </p>
+  </div>
+  <div class="ui vertical divider"></div>
+  <div class="center aligned column">
+   <p> Gerencia de operaciones <br>
+      Fono : +569 50047696 <br>
+      shernandez@serenamarmol.cl
+   </p>
+  </div>
+</div>                              
+    </div>
+
+    cuarzoo
+
+    <div class="ui raised segment">
+      <a class="ui green ribbon label">Contactanos</a>
+      <span>Titulo</span>
+      
+      <p><br>
+      Estamos ubicados en <br>
+      Ruta D-43, Kilometro 12, Numero 5000 <br>
+      Sector Pan de Azúcar, <br>
+      Coquimbo, IV Regió.
+      </p>
+      
+      
+      
+      <div class="ui two column middle aligned very relaxed stackable grid">
+  <div class="column">
+  <p>Administración y Ventas <br>
+         Fono : +569 50047894 <br>
+         contacto@serenamarmol.cl
+      </p>
+  </div>
+  <div class="ui vertical divider"></div>
+  <div class="center aligned column">
+   <p> Gerencia de operaciones <br>
+      Fono : +569 50047696 <br>
+      shernandez@serenamarmol.cl
+   </p>
+  </div>
+</div>                              
+    </div>
+
+    granitooo
+
+    <div class="ui raised segment">
+      <a class="ui green ribbon label">Contactanos</a>
+      <span>Titulo</span>
+      
+      <p><br>
+      Estamos ubicados en <br>
+      Ruta D-43, Kilometro 12, Numero 5000 <br>
+      Sector Pan de Azúcar, <br>
+      Coquimbo, IV Regió.
+      </p>
+      
+      
+      
+      <div class="ui two column middle aligned very relaxed stackable grid">
+  <div class="column">
+  <p>Administración y Ventas <br>
+         Fono : +569 50047894 <br>
+         contacto@serenamarmol.cl
+      </p>
+  </div>
+  <div class="ui vertical divider"></div>
+  <div class="center aligned column">
+   <p> Gerencia de operaciones <br>
+      Fono : +569 50047696 <br>
+      shernandez@serenamarmol.cl
+   </p>
+  </div>
+</div>                              
+    </div>
+
+
+    Gold!!
+
+    <div class="ui raised segment">
+      <a class="ui green ribbon label">Contactanos</a>
+      <span>Titulo</span>
+      
+      <p><br>
+      Estamos ubicados en <br>
+      Ruta D-43, Kilometro 12, Numero 5000 <br>
+      Sector Pan de Azúcar, <br>
+      Coquimbo, IV Regió.
+      </p>
+      
+      
+      
+      <div class="ui two column middle aligned very relaxed stackable grid">
+  <div class="column">
+  <p>Administración y Ventas <br>
+         Fono : +569 50047894 <br>
+         contacto@serenamarmol.cl
+      </p>
+  </div>
+  <div class="ui vertical divider"></div>
+  <div class="center aligned column">
+   <p> Gerencia de operaciones <br>
+      Fono : +569 50047696 <br>
+      shernandez@serenamarmol.cl
+   </p>
+  </div>
+</div>                              
+    </div>
 
 <?php include_once "footer.php";?>
