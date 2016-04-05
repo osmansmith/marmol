293d65395ad44ef0358fb013c32ca8f4d777f617
CuscarisunWeb <jean.veliz.cortes@gmail.com>
2016-04-05 04:36:28 -0300

    Cuarzoo!!

--- cuarzo.php
+++ cuarzo.php
@@ -12,24 +12,72 @@
       Coquimbo, IV Regió.
       </p>
       
-      
-      
-      <div class="ui two column middle aligned very relaxed stackable grid">
-  <div class="column">
-  <p>Administración y Ventas <br>
-         Fono : +569 50047894 <br>
-         contacto@serenamarmol.cl
-      </p>
-  </div>
-  <div class="ui vertical divider"></div>
-  <div class="center aligned column">
-   <p> Gerencia de operaciones <br>
-      Fono : +569 50047696 <br>
-      shernandez@serenamarmol.cl
-   </p>
-  </div>
-</div>                              
-    </div>
+      <!-- Divisores -->
+
+      <div class="ui ten column middle aligned very relaxed stackable grid">
+        <div class="column">
+          <p>Administración y Ventas <br>
+              Fono : +569 50047894 <br>
+              contacto@serenamarmol.cl
+          </p>
+        </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones9 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+
+      <div class="center aligned column">
+       <p> Gerencia de operaciones8 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones7 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones6 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones5 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones4 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones3 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones2 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+      <div class="center aligned column">
+       <p> Gerencia de operaciones1 <br>
+          Fono : +569 50047696 <br>
+          shernandez@serenamarmol.cl
+       </p>
+      </div>
+          </div>                            
+        </div>
 
     cuarzoo
 
