c8a68fcaab5fe96a9c8a140f9d48997830c578aa
osmansmith <osman_ahumada@hotmail.com>
2016-04-05 02:42:05 -0300

    FALTA EL MAPA
    falta mapa de localizacion

--- contactos.php
+++ contactos.php
@@ -1,6 +1,62 @@
 <?php include_once "head.php";?>
 
 <!-- Contenido de la pagina-->
-<div id="map"></div>
+<div class="ui two column grid">
+<div class="column">
+    <div class="ui raised segment">
+      <a class="ui green ribbon label">Contactanos</a>
+      <span>Titulo</span>
+      
+      <p><br>
+      Estamos ubicados en <br>
+      Ruta D-43, Kilometro 12, Numero 5000 <br>
+      Sector Pan de Azúcar, <br>
+      Coquimbo, IV Regió.
+      </p>
+      
+      
+      
+      <div class="ui two column middle aligned very relaxed stackable grid">
+  <div class="column">
+  <p>Administración y Ventas <br>
+         Fono : +569 50047894 <br>
+         contacto@serenamarmol.cl
+      </p>
+  </div>
+  <div class="ui vertical divider"></div>
+  <div class="center aligned column">
+   <p> Gerencia de operaciones <br>
+      Fono : +569 50047696 <br>
+      shernandez@serenamarmol.cl
+   </p>
+  </div>
+</div>                              
+    </div>
+  </div>
+  <div class="column">
+    
+     <form class="ui form">
+  <div class="field">
+    <!--<label>Nombre :</label>-->
+    <input type="text" name="nombre" placeholder="NOMBRE">
+  </div>
+  <div class="field">
+    <!--<label>Correo :</label>-->
+    <input type="email" name="correo" placeholder="CORREO">
+  </div>
+  <div class="field">
+    <input type="number" name="fono" placeholder="TELEFONO">
+  </div>
+  <div class="field">
+      <textarea name="mensaje" id="mensaje" cols="30" rows="10" placeholder="MENSAJE"></textarea>
+  </div>
+  <button class="ui button" type="submit">ENVIAR</button>
+   <button class="ui button" type="reset">LIMPIAR</button>
+  
+</form>
+ 
+  </div>
+</div>
+
 
 <?php include_once "footer.php";?>
