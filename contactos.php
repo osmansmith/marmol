d38395caf01737820a349d006988090c22952c03
CuscarisunWeb <jean.veliz.cortes@gmail.com>
2016-04-05 07:21:05 -0300

    Mapa

--- contactos.php
+++ contactos.php
@@ -63,33 +63,10 @@
 </div>                              
     </div>
     <div class="ui raised segment">
-      <a class="ui green ribbon label">Contactanos</a>
-      <span>Titulo</span>
+     
       
-      <p><br>
-      Estamos ubicados en <br>
-      Ruta D-43, Kilometro 12, Numero 5000 <br>
-      Sector Pan de Azúcar, <br>
-      Coquimbo, IV Regió.
-      </p>
-      
-      
-      
-      <div class="ui two column middle aligned very relaxed stackable grid">
-  <div class="column">
-  <p>Administración y Ventas <br>
-         Fono : +569 50047894 <br>
-         contacto@serenamarmol.cl
-      </p>
-  </div>
-  <div class="ui vertical divider"></div>
-  <div class="center aligned column">
-   <p> Gerencia de operaciones <br>
-      Fono : +569 50047696 <br>
-      shernandez@serenamarmol.cl
-   </p>
-  </div>
-</div>                              
+<img src="/img/mapa.jpg">
+
     </div>
   </div>
   <div class="column">
